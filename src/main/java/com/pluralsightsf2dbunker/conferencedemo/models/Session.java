9f9b7272b34a572bdd6123a688cae984516762f1
wrafalit <wrafalit@gmail.com>
2022-06-10 23:39:56 +0200

    Initial commit

---
+++ src/main/java/com/pluralsightsf2dbunker/conferencedemo/models/Session.java
@@ -0,0 +1,69 @@
+package com.pluralsightsf2dbunker.conferencedemo.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity(name="sessions")
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "session_id", nullable = false)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id"))
+    List<Speaker> speakers;
+
+    public Session() {
+
+    }
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
+    }
+
+    public String getSession_name() {
+        return session_name;
+    }
+
+    public void setSession_name(String session_name) {
+        this.session_name = session_name;
+    }
+
+    public String getSession_description() {
+        return session_description;
+    }
+
+    public void setSession_description(String session_description) {
+        this.session_description = session_description;
+    }
+
+    public Integer getSession_length() {
+        return session_length;
+    }
+
+    public void setSession_length(Integer session_length) {
+        this.session_length = session_length;
+    }
+
+    private Integer session_length;
+
+    public Long getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(Long session_id) {
+        this.session_id = session_id;
+    }
+
+}
